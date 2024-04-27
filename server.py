f1b717b2b406c590f02c6c5d3c4551c82050bc94
kovalyov-z3 <71659839+kovalyov-z3@users.noreply.github.com>
2024-04-27 19:10:11 +0300

    first commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/", methods= ["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("login.html", username = "")
+
+    if request.method == "POST":
+
+        login = request.form.get("login")
+        pasword = request.form.get("password")
+        return render_template("login.html", username = "Привет " + login)
+
+@app.route("/snake_to_space", methods= ["GET", "POST"])
+def snake():
+    return {"status":"launched!", "description":"from timeweb apps"}
+
+app.run()
